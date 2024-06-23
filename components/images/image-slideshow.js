df5f44baa74a141a1de623039b951b5f52718e8b
Herlea Marius <mariusherlea@gmail.com>
2024-06-23 12:20:44 +0300

    add client side slideshow

--- components/images/image-slideshow.js
+++ components/images/image-slideshow.js
@@ -1,6 +1,6 @@
+"use client";
 import { useEffect, useState } from "react";
 import Image from "next/image";
-
 import burgerImg from "@/assets/burger.jpg";
 import curryImg from "@/assets/curry.jpg";
 import dumplingsImg from "@/assets/dumplings.jpg";
