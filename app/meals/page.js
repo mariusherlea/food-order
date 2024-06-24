e386b2983d822ca1860f305a0a70bc165aeed3f4
Herlea Marius <mariusherlea@gmail.com>
2024-06-24 10:12:18 +0300

    populate shared meals with db results

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,28 @@
-export default function Meals() {
-  return <h1>Meals</h1>;
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+import { getMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>{" "}
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun !
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 }
