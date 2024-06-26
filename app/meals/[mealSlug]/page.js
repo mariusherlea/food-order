c7ba7885149a84b02d0be2e07eb8b974683e40b7
Herlea Marius <mariusherlea@gmail.com>
2024-06-26 15:34:26 +0300

    move images to aws

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -15,7 +15,7 @@ export default function MealDetailsPage({ params }) {
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill />
+          <Image src={`https://mariusherlea-recipe-share.s3.eu-north-1.amazonaws.com/${meal.image}`} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
